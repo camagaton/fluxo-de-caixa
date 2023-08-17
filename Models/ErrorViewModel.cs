c6ce0f271275597ce27c0be92a3cc699a4d439fb
camagaton <camagaton@gmail.com>
2023-08-17 14:37:15 -0300

    criacao do programa fluxo de caixa dotnet

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace fluxo_de_caixa.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
